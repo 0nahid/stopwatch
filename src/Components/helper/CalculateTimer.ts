9c5daef856a567ac6c9cd009f8b254ff0e66580e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-04 14:13:50 +0600

    issue:fixed

--- src/Components/helper/CalculateTimer.ts
+++ src/Components/helper/CalculateTimer.ts
@@ -2,9 +2,11 @@ function calculateTimer(timeInSeconds: number): Array<number | string> {
   const hour: number = Math.floor(timeInSeconds / 3600);
   const minute: number = Math.floor((timeInSeconds - hour * 3600) / 60);
   const second: number = timeInSeconds - hour * 3600 - minute * 60;
+  const nanoSecond = Math.floor((second - Math.floor(second)) * 1000);
   const hourFormat: string = hour < 10 ? `0${hour}` : `${hour}`;
   const minuteFormat: string = minute < 10 ? `0${minute}` : `${minute}`;
   const secondFormat: string = second < 10 ? `0${second}` : `${second}`;
-  return [hourFormat, minuteFormat, secondFormat];
+  const nanoSecondFormat: string = nanoSecond < 10 ? `00${nanoSecond}` : nanoSecond < 100 ? `0${nanoSecond}` : `${nanoSecond}`;
+  return [hourFormat, minuteFormat, secondFormat,nanoSecondFormat];
 }
 export default calculateTimer;
