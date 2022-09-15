8c275106665141aa4d7c700e755b7ea3c3bee598
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-09-16 02:34:47 +0600

    changes ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { createContext, useEffect, useState } from "react";
 import { Toaster } from 'react-hot-toast';
-
 import Timer from "./Components/Timer/Timer";
 import './index.css';
 
+export const InitializeContext = createContext({} as any);
+
 export default function App() {
+    const [theme, setTheme] = useState<boolean>(false);
+
+    useEffect(() => {
+        setTheme(JSON.parse(window.localStorage.getItem("theme") || "false"));
+    }, [])
+
+    const handleThemeChange: () => void = () => {
+        setTheme(!theme);
+        window.localStorage.setItem("theme", JSON.stringify(!theme));
+    }
     return (
-        <div className="bg-slate-900/10 ">
-            <Timer />
-            <Toaster />
+        <div data-theme={
+            theme ? "night" : "light"
+        } className={`${theme ? "bg-[url('https://i.ibb.co/y8QMy2P/bg-dar-md.jpg')]" : "bg-[url('https://i.ibb.co/r66ZP7V/bg-light-md.jpg')]"} h-screen bg-cover overflow-hidden`}>
+            <InitializeContext.Provider value={{
+                theme,
+                handleThemeChange
+            }}>
+                <Timer />
+                <Toaster />
+            </InitializeContext.Provider>
         </div>
     )
 }
